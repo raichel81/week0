eea4d7739553e08fbe45cfdb1609783199c24daf
Rachel <rai.chel.ann@gmail.com>
2017-08-08 14:02:43 -0700

    week 0
    Practice algorithms

---
+++ tester.js
@@ -0,0 +1,76 @@
+/*console.log('hi there');
+
+var num = 12;
+console.log(num);
+
+for(var i = 1; i <= 10; i++) {
+    console.log(i);
+}   
+let counter = 1;
+while (counter <= 10){
+    console.log(counter);
+    if (counter < 5) {
+        counter ++;
+    } else {
+        counter += 3;
+    }
+    counter ++;
+}
+
+for(var i = 1; i <= 1000; i ++){
+    if(i % 4 == 0){
+        console.log('divisible by 4');
+    } if(i % 5 == 0){
+        console.log('divisible by 5');
+    } else{
+        console.log(i);
+    }
+}
+
+for(var i = 1; i <= 2017; i ++){
+    if(i % 100 !== 0 && i % 4 == 0 || i % 400 == 0){
+        console.log(i + " is a leap year");
+    } else{
+        // console.log(i);
+    }
+}
+
+
+function CreateArr(){
+    var arr = [];
+    for(i=1; i<=255; i++){
+        arr[i-1]=i;
+    }
+    console.log (arr);
+}
+
+CreateArr();
+
+function AddingArr(arr){
+    var total = 0
+    for(var i = 0; i<arr.length; i++){
+        total += arr[i];
+    }
+    return total;
+}
+console.log(AddingArr([2,4,9,1,7]));
+*/
+
+function AddingArr(arr){
+    var total = 0
+    for(var i = 0; i<arr.length; i++){
+        total += arr[i];
+    }
+    return total;
+}
+
+
+
+
+
+
+
+
+
+
+
